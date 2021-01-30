eed72f017f0b8f83cf00cd027c1f227a265ef7d3
Martín Ferrarese <martin.ferrarese@grupoesfera.com.ar>
2021-01-30 01:08:01 -0300

    Agrega productos y obtiene todos los productos de la base de datos

---
+++ deluphotos/src/main/java/com/deluphotos/deluphotos/Entidad/Producto.java
@@ -0,0 +1,43 @@
+package com.deluphotos.deluphotos.Entidad;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private Long precio;
+    private String descripción;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Long precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+}
---
+++ deluphotos/src/main/java/com/deluphotos/deluphotos/Repositorio/RepositorioDeProducto.java
@@ -0,0 +1,9 @@
+package com.deluphotos.deluphotos.Repositorio;
+
+import com.deluphotos.deluphotos.Entidad.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepositorioDeProducto extends CrudRepository<Producto, Integer> {
+
+
+}
