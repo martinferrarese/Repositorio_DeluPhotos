5af5bc61ae7a5db49b54764bb0cb0cf41dfdab36
Martín Ferrarese <martin.ferrarese@grupoesfera.com.ar>
2021-03-28 00:12:00 -0300

    Agrega carousel de imágenes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,85 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
 
+  dynamicSlides = [
+    {
+      id: "1",
+      src:'./assets/img/delu1.jpg',
+      alt:'Pulsera',
+      title:'Pulsera',
+      url: "http://google.com.ar"
+    },
+    {
+      id: "2",
+      src:'./assets/img/delu2.jpg',
+      alt:'Golosinas',
+      title:'Golosinas'
+    },
+    {
+      id: "3",
+      src:'./assets/img/delu3.jpg',
+      alt:'Cuadrito',
+      title:'Cuadrito'
+    },
+    {
+      id: "4",
+      src:'./assets/img/delu4.jpg',
+      alt:'Remera',
+      title:'Remera'
+    },
+    {
+      id: "5",
+      src:'./assets/img/delu5.jpg',
+      alt:'Cuadrito dibujado',
+      title:'Cuadrito dibujado'
+    },
+    {
+      id: "6",
+      src:'./assets/img/delu6.jpg',
+      alt:'Calendario',
+      title:'Calendario'
+    },
+    {
+      id: "7",
+      src:'./assets/img/delu7.jpg',
+      alt:'Navidad',
+      title:'Navidad'
+    },
+    {
+      id: "8",
+      src:'./assets/img/delu8.jpg',
+      alt:'Taza',
+      title:'Taza'
+    }
+  ]
+
   constructor(private http: HttpClient){}
 
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 900,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    navText: ['&#8249', '&#8250;'],
+    responsive: {
+      0: {
+        items: 1 
+      },
+      400: {
+        items: 2
+      },
+      760: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    },
+    nav: false
+  }
 
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { MaterialModule } from './material/material.module';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
